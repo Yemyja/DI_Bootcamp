d0885fa4e4415a15436ac386d7001fb031f97a89
Yemyja <yemyja@gmail.com>
2023-04-27 18:15:10 -0600

    first commit

---
+++ Week8/Day3/exercises.js
@@ -0,0 +1,14 @@
+let escue= {
+
+}
+let keys= Object.keys(familia);// object siempre se escribe con mayuscula
+console.log(keys);//aqui me va a dar familiar de todos
+
+let vals= Object.values(familia);
+console.log(vals);//aqui me va a dar nombre de todos
+
+let entries= Object.entries(familia);
+console.log(entries); //aqui me va a dar familiar y nombre de todos
+
+console.log (entries [2][1]);//aqui me da a dar hija1,Yael
+console.log (Object.entries(familia)) //aqui me va a dar familiar y nombre de todos
