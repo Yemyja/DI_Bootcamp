318836417459d85590ff105ac7b2c58c7787f114
Yemyja <yemyja@gmail.com>
2023-08-15 20:30:15 -0600

    first

--- Week18/Day5/Redux de Daily-MovieApp/movie-app/src/App.js
+++ Week18/Day5/Redux de Daily-MovieApp/movie-app/src/App.js
@@ -1,64 +1,63 @@
-
-import { useState, useEffect } from 'react'
-import React from 'react'
-import "./App.css"
+import React, { useEffect, useState } from 'react'; // Import useState
+import { connect } from 'react-redux';
+import { setMovies } from './actions/movies';
+import { setSearchResults } from './actions/search';
+import './App.css';
 import MovieCard from './MovieCard';
 
-
-function App() {
-
+function App({ movies, setMovies, searchResults, setSearchResults }) {
   const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=4ef3f36bc9a885c3d3a2664a78f8520a";
   const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=4ef3f36bc9a885c3d3a2664a78f8520a&query=";
-  
-  const [movies, setMovies] = useState([]);
-  const [term, setTerm] = useState([]);
+
+  const [term, setTerm] = useState('');
 
   useEffect(() => {
     fetch(API_URL)
-    .then(res => res.json())
-    .then(data => setMovies(data.results))
-  }, [])
-
-  console.log(movies);
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, [setMovies]);
 
   const handleSearch = (e) => {
     e.preventDefault();
     fetch(API_SEARCH + term)
-      .then(res => res.json())
-      .then(data => setMovies(data.results))
-  }
+      .then((res) => res.json())
+      .then((data) => setSearchResults(data.results));
+  };
 
-  const handleGoBack = () => {
-    window.location.reload();
-  
+  const handleInputChange = (e) => {
+    setTerm(e.target.value);
   };
 
   return (
     <div className='App'>
-      <div className='title1'>
-            <h1>Peliculas del momento</h1>
-          </div>
       <div className='search_nav'>
-          
-<br></br>
-          <div className='search_box'>
-            <form onSubmit={handleSearch}>
-              <input onChange={(e) => setTerm(e.target.value)}/>
-              <button>Search</button>
-            </form>
-          </div>
-          <div className= 'go-back'>
-          <button onClick={handleGoBack}>Go back</button>
-          </div>
+        <div className='title'>
+          <h1>DaveFlix</h1>
+        </div>
+        <div className='search_box'>
+          <form onSubmit={handleSearch}>
+            <input onChange={handleInputChange} />
+            <button>Search</button>
+          </form>
+        </div>
       </div>
-
       <div className='movies'>
-        {movies.map((movie) => (
-          <MovieCard {...movie} /> 
+        {(term ? searchResults : movies).map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  movies: state.movies,
+  searchResults: state.searchResults,
+});
+
+const mapDispatchToProps = {
+  setMovies,
+  setSearchResults,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ Week18/Day5/Redux de Daily-MovieApp/movie-app/src/actions/search.js
@@ -0,0 +1,7 @@
+
+export const setSearchResults = (results) => {
+    return {
+      type: 'SET_SEARCH_RESULTS',
+      payload: results,
+    };
+  };
